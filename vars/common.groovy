3dfc9683c740f9fcd565e1926e9ff06802d8877b
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-04-27 22:15:55 +0530

    jenkinsfile

--- vars/common.groovy
+++ vars/common.groovy
@@ -4,11 +4,32 @@ def compile() {
     }
 
     if ( app_lang == "maven"){
-        sh 'mvn package'
+        sh 'mvn clean compile package'
     }
 
     if ( app_lang == "golang"){
         sh 'install golang'
     }
 
+}
+
+def unittests() {
+
+    if (app_lang == "nodejs") {
+        // Developer is missing unit test cases in our project, He need to add them as best practice, We are skipping to proceed further
+        sh 'npm test || true'
+
+    }
+
+    if (app_lang == "maven") {
+        sh 'mvn test'
+    }
+
+    if (app_lang == "python") {
+        sh 'python3 -m unittest'
+    }
+}
+
+def email(email_note) {
+    mail bcc: '', body: "Job Failed - ${JOB_BASE_NAME}\nJenkins URL - ${JOB_URL}", cc: '', from: 'surendrapandrinki17@gmail.com', replyTo: '', subject: "Jenkins Job Failed - ${JOB_BASE_NAME}", to: 'surendrapandrinki17@gmail.com'
 }

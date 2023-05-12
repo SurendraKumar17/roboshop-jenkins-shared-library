611084ff1f09d60a18bb71f683ddb4868a29fedf
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-05-12 12:25:15 +0530

    ,,,

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -17,11 +17,10 @@ def call() {
              }
 
              stage('Quality Control') {
-                 sh "sonar-scanner -Dsonar.host.url=http://172.31.3.24:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component}"
-                 //SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-                 //SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                 SONAR_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+                 SONAR_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
                  //wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-                 // sh "sonar-scanner -Dsonar.host.url=http://172.31.3.24:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
+                 sh "sonar-scanner -Dsonar.host.url=http://172.31.3.24:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} //-Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
                  //sh "echo Sonar Scan"
              }
          }

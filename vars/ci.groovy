c0590550d55786bf97dabfccf4e491b3b5a0532b
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-05-18 15:58:15 +0530

    ,,,

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -40,21 +40,18 @@ def call() {
               }
             }
 
-            stage('Upload Code to Centralized Place') {
-                common.artifactPush()
+
+            if (app_lang == "maven") {
+                stage('Build Package') {
+                    sh "mvn package && cp target/${component}-1.0.jar ${component}.jar"
+                }
             }
 
-             //if (app_lang == "maven") {
-              //  stage('Build Package') {
-               //     sh "mvn package && cp target/${component}-1.0.jar ${component}.jar"
-               // }
-          //  }
-
-            // if(env.PUSH_CODE == "true") {
-            //stage('Upload Code to Centralized Place') {
-            //    common.artifactPush()
-           // }
-       // }
+            if(env.PUSH_CODE == "true") {
+                stage('Upload Code to Centralized Place') {
+                    common.artifactPush()
+                }
+            }
 
 
         }

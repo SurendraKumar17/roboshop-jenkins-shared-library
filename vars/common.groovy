189c6bf00ae00a0146097ca692ebbf4fd417f143
Learn_shell <surendrakumar.pandrinki@gmail.com>
2023-05-20 12:31:58 +0530

    ,,,.

--- vars/common.groovy
+++ vars/common.groovy
@@ -36,7 +36,15 @@ def email(email_note) {
 def artifactPush() {
     sh "echo ${TAG_NAME} >VERSION"
     if (app_lang == "nodejs") {
-        sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION"
+        sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${extraFiles}"
+    }
+
+    if (app_lang == "nginx" || app_lang == "python") {
+     sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile ${extraFiles}"
+    }
+
+    if (app_lang == "maven") {
+     sh "zip -r ${component}-${TAG_NAME}.zip * ${component}.jar VERSION ${extraFiles}"
     }
 
     NEXUS_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names nexus.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
